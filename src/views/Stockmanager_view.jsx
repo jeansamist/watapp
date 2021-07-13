e0208f7591fb8eaa90399a77bcf125c74a4a8adb
BAHA Ephraïm <jeansamist@gmail.com>
2021-07-13 20:04:16 +0100

    New app

--- src/views/Stockmanager_view.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class Stockmanager_view extends React.Component {
-  render () {
-    return <div className="stockmanager view">
-      Stock
-    </div>
-  }
-}
