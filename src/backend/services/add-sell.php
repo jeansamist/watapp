4e885cde78a40e0f524fdcd4242b16f439670098
Alfred <kamnoautrylucienne@gmail.com>
2021-08-17 11:36:41 +0100

    faire une vente

--- src/backend/services/add-sell.php
+++ src/backend/services/add-sell.php
@@ -4,3 +4,40 @@ include_once '../config/headers.php';
 include_once '../config/bd.php';
 include_once '../config/classes/Autoloader.php';
 Autoloader::register();
+
+if (!empty($_POST)) {
+    $error = false;
+    foreach ($_POST as $post_data) {
+      if ($post_data == "") {
+        $error = true;
+      }
+    }
+    if (!$error) {
+      $clientName =  htmlspecialchars($_POST['clientName']);
+      $productName =  htmlspecialchars($_POST['productName']);
+      foreach ($productName as $value) {
+        $value = (int) $value;
+        $getProductIdReq = $pdo->prepare('SELECT id FROM stocks WHERE name = ?');
+        $getProductIdReq->execute([$value]);
+        $fetch = $getProductIdReq->fetch();
+      }
+      var_dump($value);
+      die();
+
+      $quantity =  htmlspecialchars($_POST['quantity']);
+      $addSell = $pdo->prepare('INSERT INTO sells (item, quantity, client) VALUES (?, ?, ?)');
+      $addSell->execute([
+        $clientName,
+        json_encode($productName),
+        $quantity
+      ]);
+      if ($addSell === false) {
+          $error = false;
+          $err = 'Une erreur est survenu pendant le traitement';
+      }
+    }
+} else {
+  $err = false;
+  $toReturn = new ReqResponse($err, 'post est vide');
+}
+echo json_encode($toReturn);
