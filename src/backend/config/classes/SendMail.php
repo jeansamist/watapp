429e117e7a87ce099dc7eab3c5eaf749696ebb37
Alfred <kamnoautrylucienne@gmail.com>
2021-08-25 14:05:23 +0100

    create user

--- src/backend/config/classes/SendMail.php
+++ src/backend/config/classes/SendMail.php
@@ -2,8 +2,8 @@
 class SendMail {
     public static function send_mail($recever, $subject, $message = "<h1>E-mail envoier par WATAPP 0.1</h1>") {
         $headers = array(
-            'From' => 'jeansamist@gmail.com',
-            'Reply-To' => 'webmaster@example.com',
+            'From' => 'watappinfo237@gmail.com',
+            'Reply-To' => 'watappinfo237@gmail.com',
             'X-Mailer' => 'PHP/' . phpversion()
         );
         if (mail($recever, $subject, $message, $headers)) {
